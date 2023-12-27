9d6d745c1a6febe90303c7c5eba46deb6ff6b9dd
Angshuman Roy <iamangshu3@gmail.com>
2023-12-27 23:18:38 +0530

    First Commit

---
+++ Basics/let-const-var_1.js
@@ -0,0 +1,5 @@
+var firstVar = "This is first variable with var";
+let secondVar = "This is second variable with let";
+const thirdVar = "This is third variable with const";
+
+console.table([firstVar, secondVar, thirdVar]);

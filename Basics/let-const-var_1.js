7e83cc76c3456b698e9822c51cbfba9379317de2
Angshuman Roy <iamangshu3@gmail.com>
2023-12-27 23:26:11 +0530

    var let const code

--- Basics/let-const-var_1.js
+++ Basics/let-const-var_1.js
@@ -3,3 +3,10 @@ let secondVar = "This is second variable with let";
 const thirdVar = "This is third variable with const";
 
 console.table([firstVar, secondVar, thirdVar]);
+
+firstVar = "This is first var 2nd time";
+secondVar = "This is second var 2nd time";
+//=== you can not redeclare const ===
+// thirdVar = "This is third var 2nd time";
+
+console.table([firstVar, secondVar, thirdVar]);
